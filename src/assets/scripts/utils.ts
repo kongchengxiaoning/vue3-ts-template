46a2abfb62a3a5102e734c10f1b18417cd048e05
冯亚宁 <fengyaning@cnb.design>
2021-09-28 14:15:30 +0800

    fix: config问题修复

--- src/assets/scripts/utils.ts
+++ src/assets/scripts/utils.ts
@@ -1,11 +1,8 @@
-import config from './config'
-const { IMG_ERROR } = config
-
 /* 本地图片地址处理 */
-export const handleImg = (img = IMG_ERROR, suffix = 'png') => {
+export const handleImg = (img = '', suffix = 'png') => {
   return `/${img}.${suffix}`
 }
 /* 服务器图片地址处理 */
-export const handleServeImg = (img = IMG_ERROR) => {
+export const handleServeImg = (img = '') => {
   return img
 }
